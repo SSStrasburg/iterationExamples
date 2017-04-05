5b95bb800704f611132a5d2a0ec1041b399137e2
Steven Strasburg <SSStrasburg@gmail.com>
2017-04-05 07:33:39 -0400

    adding Fizzbuzz

---
+++ fizzbuzz/js/fizzbuzz.js
@@ -0,0 +1,19 @@
+
+
+function fizzbuzz(){
+
+for(var i, i < fizzbuzz().length, i++){
+  output = '';
+
+  if(i%3 === 0){
+    output += 'Fizz'  // if multiple of 3
+
+  } if(i%5 === 0){
+    output += 'Buzz'  // if multiple of 5
+
+  } else{
+    print(output);    // output any number other than a multiple of 3 or 5
+  }
+}
+
+};
--- js/main.js
+++ js/main.js
@@ -6,12 +6,13 @@
   let paragraph = 'practice practice practice makes perfect time is of the essence no time like the present many more miles before I rest before I rest many more miles';
   //console.log(paragraph);  // my random paragraph
 
-  let arrayOfStrings = paragraph.split(' ');
+  let arrayOfStrings = paragraph.split(' '); // This makes the string and puts it into an array
   //console.log(arrayOfStrings);  //
 
   let results = {};
 
-
+// this array iterates though the array of words.  checks if it is new to the array then (adds or counts) the word in an object
+//
 //   for(var i = 0; i < arrayOfStrings.length; i++) {
 //     if(!results[arrayOfStrings[i]]){
 //       //console.log(results[arrayOfStrings[i]]);
@@ -29,7 +30,7 @@ arrayOfStrings.forEach(function(word){
   } else{
     results[word] = 1;
   }
-  
+
 });
 console.log(results);
 
